365535968cd3609756491c3ae4ddb276fa51c74b
Saintone11 <ysw9708@naver.com>
2023-03-10 02:14:54 +0900

    update

---
+++ src/main/java/com/cos/blog/model/Board.java
@@ -0,0 +1,47 @@
+package com.cos.blog.model;
+
+import java.sql.Timestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.Lob;
+import javax.persistence.ManyToOne;
+
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.CreationTimestamp;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+public class Board {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@Column(nullable = false, length = 100)
+	private String title;
+	
+	@Lob // 대용량 데이터
+	private String content; //섬머노트 라이브러리
+	
+	@ColumnDefault("0")
+	private int count;
+	
+	@ManyToOne
+	@JoinColumn(name = "userid")
+	private User user; // DB는 오브젝트를 저장할 수 없다. FK
+	
+	@CreationTimestamp
+	private Timestamp createDate;
+}
---
+++ src/main/java/com/cos/blog/model/User.java
@@ -0,0 +1,44 @@
+package com.cos.blog.model;
+
+import java.sql.Timestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.CreationTimestamp;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity // User 클래스가 MySQL에 테이블이 생성이 된다.
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@Column(nullable = false, length = 30)
+	private String username;
+	
+	@Column(nullable = false, length = 100)
+	private String password;
+	 
+	@Column(nullable=false, length = 50)
+	private String myEmail;
+	
+	@ColumnDefault("'user'")
+	private String role; // Enum을 쓰는게 좋다. //admin, user, manager
+	
+	@CreationTimestamp // 시간 자동입력
+	private Timestamp createDate;
+}  
